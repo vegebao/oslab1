d6992a6629638bf5576906f63da0dd5074afa22b
Zhao Jinwei <487394112@qq.com>
2023-04-01 10:08:36 +0800

    before init the section register in lab1.2

--- lab1-STUID/bootloader/start.s
+++ lab1-STUID/bootloader/start.s
@@ -1,6 +1,6 @@
 # TODO: This is lab1.1
 /* Real Mode Hello World */
- .code16
+/* .code16
  .global start
  start:
  	movw %cs, %ax
@@ -29,66 +29,66 @@
  	int $0x10
  	popw %bp
  	ret
-
+*/
 
 
 # TODO: This is lab1.2
 /* Protected Mode Hello World */
-#  .code16
-#  .global start
-#  start:
-#      cli                             #关闭中断
-#                                      #启动A20总线
+  .code16
+  .global start
+  start:
+      cli                             #关闭中断
+                                      #启动A20总线
     
     
-#      data32 addr32 lgdt gdtDesc      #加载GDTR
-#                                      #启动保护模式
+      data32 addr32 lgdt gdtDesc      #加载GDTR
+                                      #启动保护模式
     
-#                                      #设置CR0的PE位（第0位）为1
-#      data32 ljmp $0x08, $start32     #长跳转切换至保护模式
+      movw %cr0, %eax                                #设置CR0的PE位（第0位）为1
+      or   $0x1, %eax
+	  movw %eax, %cr0
+      data32 ljmp $0x08, $start32     #长跳转切换至保护模式
 
-#  .code32
-#  start32:
+  .code32
+  start32:
 #      ...                             #初始化DS ES FS GS SS 初始化栈顶指针ESP
 
-#  	pushl $13 # pushing the size to print into stack
-#  	pushl $message # pushing the address of message into stack
-#  	calll displayStr # calling the display function
-# loop:
-#  	jmp loop
+  	pushl $13 # pushing the size to print into stack
+  	pushl $message # pushing the address of message into stack
+  	calll displayStr # calling the display function
+ loop:
+  	jmp loop
 
-# message:
-# 	.string "Hello, World!\n\0"
+ message:
+ 	.string "Hello, World!\n\0"
 
-# displayStr:
-# 	movl 4(%esp), %ebx
-# 	movl 8(%esp), %ecx
-# 	movl $((80*5+0)*2), %edi
-# 	movb $0x0c, %ah
-# nextChar:
-# 	movb (%ebx), %al
-# 	movw %ax, %gs:(%edi)
-# 	addl $2, %edi
-# 	incl %ebx
-# 	loopnz nextChar # loopnz decrease ecx by 1
-# 	ret
+ displayStr:
+ 	movl 4(%esp), %ebx
+ 	movl 8(%esp), %ecx
+ 	movl $((80*5+0)*2), %edi
+ 	movb $0x0c, %ah
+ nextChar:
+ 	movb (%ebx), %al
+ 	movw %ax, %gs:(%edi)
+ 	addl $2, %edi
+ 	incl %ebx
+ 	loopnz nextChar # loopnz decrease ecx by 1
+ 	ret
 
-# gdt:
-#     .word                           #GDT第一个表项必须为空
-#     .byte 
-#     .word                           #代码段描述符
-#     .byte 
-        
-#     .word                           #数据段描述符
-#     .byte 
-        
-#     .word                           #视频段描述符
-#     .byte 
+ gdt:
+     .quad  0x0000000000000000                         #GDT第一个表项必须为空
+     .quad  0x00af9a000000ffff                         #代码段描述符
+     .quad  0x00cf92000000ffff                         #data section discription symbol
+     .quad  0x0080890000000000                      
+     .quad  0x0000000000000000
 #     ...
 
-# gdtDesc:
-#     .word (gdtDesc - gdt -1)
-#     .long gdt
+ gdtDesc:
+     .word (gdtDesc - gdt -1)
+     .long gdt
+
+
+
 
 
 
